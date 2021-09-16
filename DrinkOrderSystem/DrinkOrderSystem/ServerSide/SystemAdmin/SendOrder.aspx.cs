93349b6335c103ac53a013121ad0bf9659984ca4
yuchi0731 <kireha2180@gmail.com>
2021-09-16 11:54:40 +0800

    更新

--- DrinkOrderSystem/DrinkOrderSystem/ServerSide/SystemAdmin/SendOrder.aspx.cs
+++ DrinkOrderSystem/DrinkOrderSystem/ServerSide/SystemAdmin/SendOrder.aspx.cs
@@ -271,48 +271,6 @@ namespace DrinkOrderSystem.ServerSide.SystemAdmin
 
 
 
-
-
-
-            //MailMessage mail = new MailMessage();
-            ////前面是發信email後面是顯示的名稱
-            //mail.From = new MailAddress(adminEmail, ademin);
-
-            ////收信者email
-            //mail.To.Add($"{mine}");
-
-            ////設定優先權
-            //mail.Priority = MailPriority.Normal;
-
-            ////標題
-            //mail.Subject = $"您好，訂購編號：{orderNumber}已訂購完成！";
-
-            ////內容
-            //mail.Body = 
-            //    "<h1>您好，您所開團的團購已訂購完成</h1><br/>" +
-            //    "並送出訂單給予廠商，待廠商送達，請依照需求時間至公司門口領取，謝謝<br/>" +
-            //    $"訂單明細為：\r\n {DetailInfo}";
-
-            ////內容使用html
-            //mail.IsBodyHtml = true;
-
-            ////設定gmail的smtp (這是google的)
-            //SmtpClient MySmtp = new SmtpClient("smtp.gmail.com", 587);
-
-            ////您在gmail的帳號密碼
-            //MySmtp.Credentials = new System.Net.NetworkCredential("kireha2180@gmail.com", "youriko0417");
-
-            ////開啟ssl
-            //MySmtp.EnableSsl = true;
-
-            ////發送郵件
-            //MySmtp.Send(mail);
-
-            ////放掉宣告出來的MySmtp
-            //MySmtp = null;
-
-            ////放掉宣告出來的mail
-            //mail.Dispose();
         }
 
 
